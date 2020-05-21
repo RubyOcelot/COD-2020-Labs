acb9508fd00d079debd0d225fd7d43cd4aba005a
RubyOcelot <1483253688@qq.com>
2020-05-21 18:17:58 +0800

    lab4:mux

--- lab4/source/mux.v
+++ lab4/source/mux.v
@@ -1,4 +1,4 @@
-module mux
+module mux_1
 #(parameter N = 32)
 (
     input i_sel,
@@ -11,6 +11,26 @@ always@(*)begin
     case (i_sel)
         1'd0: m_r=num0;
         1'd1: m_r=num1;
+        default: begin end
+    endcase
+end
+endmodule 
+
+module mux_2
+#(parameter N = 32)
+(
+    input [1:0]i_sel,
+    input [N-1:0]num0,num1,num2,num3,
+    output [N-1:0]o_m
+);
+reg [N-1:0]m_r;
+assign o_m=m_r;
+always@(*)begin
+    case (i_sel)
+        2'd0: m_r=num0;
+        2'd1: m_r=num1;
+        2'd2: m_r=num2;
+        2'd3: m_r=num3;
         default: begin end
     endcase
 end
